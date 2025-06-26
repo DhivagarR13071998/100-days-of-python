a01f8a5be125263df79a4c2102b1c2e8838f9ace
Dhivagar R <dhivagarr98@gmail.com>
2025-06-26 10:06:24 +0530

    Work in progress - before pull

--- Day 3/If Else/task.py
+++ Day 3/If Else/task.py
@@ -1,32 +1,5 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 
-# EQUAL TO : ==
-if height == 120:
-    print("you can ride the rollercoaster")
-else :
-    print("Sorry your height is not enough to ride the rollercoaster")
-
-# NOT EQUAL TO : !=
-if height != 120:
-    print("you can ride the rollercoaster")
-else :
-    print("Sorry your height is not enough to ride the rollercoaster")
-
-# Less than or equal to : <=
-if height <= 120:
-    print("you can ride the rollercoaster")
-else :
-    print("Sorry your height is not enough to ride the rollercoaster")
-
-# Greater than or equal to : >=
-if height >= 120:
-    print("you can ride the rollercoaster")
-else :
-    print("Sorry your height is not enough to ride the rollercoaster")
-
-# Less than : <
-if height < 120:
-    print("you can ride the rollercoaster")
-else :
-    print("Sorry your height is not enough to ride the rollercoaster")
+if height >120:
+    print("")
